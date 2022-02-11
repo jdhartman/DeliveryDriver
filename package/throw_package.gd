fb744db44984446918ceff9bac152fec85bbecdd
Justin Hartman <hartma62@purdue.edu>
2022-02-11 16:59:43 -0500

    Improve steering / throwing mechanics

--- package/throw_package.gd
+++ package/throw_package.gd
@@ -1,59 +1,55 @@
-extends "res://car/car.gd"
+extends Spatial
 
 export (PackedScene) var package_scene
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-onready var throw_left_target = $ThrowLeftTarget
-onready var throw_right_target = $ThrowRightTarget
+onready var throw_target = $ThrowLeftTarget
+onready var car = get_node("../Car")
+onready var throw_origin = get_node("../Car/ThrowLeft")
+
+var viewport_size = 0;
+var max_throw_distance = 200;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var throw_left = Input.is_action_just_released("throw_left")
-	var throw_right = Input.is_action_just_released("throw_right")
+func _physics_process(_delta):
+	var throw = Input.is_action_just_released("throw")
 
-	throw_left_target.visible = Input.is_action_pressed("throw_left")
-	throw_right_target.visible = Input.is_action_pressed("throw_right")
+	var mouse_position = get_viewport().get_mouse_position()
 
-	# var is_throwing = Input.is_action_pressed("throw_left")
-	# if is_throwing:
-	# 	var v = 23
+	var ray_length = 1000
+	var camera = get_node("../Camera")
+	var from = camera.project_ray_origin(mouse_position)
+	var to = from + camera.project_ray_normal(mouse_position) * ray_length
 
-	# 	var driver_origin = self.global_transform.origin
-	# 	var throw_origin = Vector3.ZERO
-	# 	throw_origin = $ThrowLeft.global_transform.origin
+	var space_state = get_world().get_direct_space_state()
+	# use global coordinates, not local to node
+	var result = space_state.intersect_ray( from, to )
 
-	# 	var d = pow(v, 2) * sin(deg2rad(170)) / 9.8
-	# 	var from_target = (throw_origin - driver_origin).normalized() * d
+	throw_target.visible = Input.is_action_pressed("throw")
 
-	# 	throwEnd.global_transform.origin = Vector3(from_target.x + throw_origin.x, throw_origin.y, from_target.z + throw_origin.z)
+	if result.has("position"):
+		throw_target.global_transform.origin = result.position
 
-
-	if throw_left || throw_right:
+	if throw:
 		var package = package_scene.instance()
 		var scene_root = get_tree().root.get_children()[0]
 
 		scene_root.add_child(package)
 
-		var driver_origin = self.global_transform.origin
-		var throw_origin = Vector3.ZERO
+		var driver_origin = car.global_transform.origin
+		var throw_vector = Vector3.ZERO
 
-		if throw_left:
-			package.global_transform = $ThrowLeft.global_transform
-			throw_origin = $ThrowLeft.global_transform.origin
+		package.global_transform = throw_origin.global_transform
+		var throw_direction = (throw_target.global_transform.origin - throw_origin.global_transform.origin).normalized()
+		throw_direction.y = 0
 
-		if throw_right:
-			package.global_transform = $ThrowRight.global_transform
-			throw_origin = $ThrowRight.global_transform.origin
+		var distance = throw_target.global_transform.origin.distance_to(throw_origin.global_transform.origin);
+		throw_vector = driver_origin + throw_direction
 
-		var from_target = (throw_origin - driver_origin).normalized()
-		print(throw_origin)
+		var from_target = (throw_vector - driver_origin).normalized()
 
-		package.apply_impulse(Vector3(0,0,0), Vector3(230 * from_target.x, 50, 230 * from_target.z))
+		package.apply_impulse(Vector3.ZERO, Vector3(distance * 15 * from_target.x, 50, distance * 15 * from_target.z))
