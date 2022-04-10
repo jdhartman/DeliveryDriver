8a197d7fe4c73f1f2589b06f3d7ebca572530827
Justin Hartman <hartma62@purdue.edu>
2022-04-10 16:31:12 -0400

    Fix meshes

--- map/floor_generation.gd
+++ map/floor_generation.gd
@@ -8,37 +8,51 @@ var mdt = MeshDataTool.new()
 export (SpatialMaterial) var material
 export var period = 0.7
 export var noise_height = 20
+export var subdivide = 64
 
 func _ready():
 	print($StaticBody/CollisionShape)
 	
 	var plane_mesh = PlaneMesh.new()
 	plane_mesh.size = Vector2(2, 2)
-	plane_mesh.subdivide_depth = 64
-	plane_mesh.subdivide_width = 64
+	plane_mesh.subdivide_depth = subdivide
+	plane_mesh.subdivide_width = subdivide
 
 	sn.period = period
 	sn.seed = randi()
 
-	st.create_from(plane_mesh, 0)
-	var array_plane = st.commit()
+	var array_plane = ArrayMesh.new()
+	array_plane.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, plane_mesh.get_mesh_arrays())
 
 	var error = mdt.create_from_surface(array_plane, 0)
+
+	$DebugNormals.clear()
 
 	for i in range(mdt.get_vertex_count()):
 		var vertex = mdt.get_vertex(i)
 		vertex.y = sn.get_noise_3dv(vertex) * noise_height
 		mdt.set_vertex(i, vertex)
+		
+		#$DebugNormals.begin(Mesh.PRIMITIVE_LINE_STRIP)
+		
+		#$DebugNormals.set_color(Color.blue)
+		#$DebugNormals.add_vertex(vertex)
+		#vertex.y += 10
+		#$DebugNormals.add_vertex(vertex)
+		
+		#$DebugNormals.end()
+		
 
 	for s in range(array_plane.get_surface_count()):
 		array_plane.surface_remove(s)
 
 	mdt.commit_to_surface(array_plane)
+	
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	st.set_material(material)
-	#st.add_smooth_group(true)
+	st.add_smooth_group(true)
+	st.generate_normals(true)
 	st.append_from(array_plane, 0, Transform.IDENTITY)
-	st.generate_normals()
 	
 	$StaticBody/FloorMesh.mesh = st.commit()
 
