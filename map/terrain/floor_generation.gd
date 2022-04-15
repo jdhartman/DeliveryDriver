3639bb597d5061626e82fd57dfd8c1d08af43c48
Justin Hartman <hartma62@purdue.edu>
2022-04-14 20:52:09 -0400

    add new foliage

---
+++ map/terrain/floor_generation.gd
@@ -0,0 +1,107 @@
+tool
+extends Spatial
+
+var sn = OpenSimplexNoise.new()
+var st = SurfaceTool.new()
+var mdt = MeshDataTool.new()
+
+export (SpatialMaterial) var material
+export var period = 0.7
+export var noise_height = 20
+export var size = 2 
+export var subdivide = 64
+
+func _ready():
+	generate_mesh()
+	
+func generate_mesh():
+	print($StaticBody/CollisionShape)
+	$DebugNormals.clear()
+	
+	var plane_mesh = PlaneMesh.new()
+	plane_mesh.size = Vector2(size, size)
+	plane_mesh.subdivide_depth = subdivide
+	plane_mesh.subdivide_width = subdivide
+
+	sn.period = period
+	sn.seed = randi()
+
+	var array_plane = ArrayMesh.new()
+	array_plane.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, plane_mesh.get_mesh_arrays())
+
+	var error = mdt.create_from_surface(array_plane, 0)
+
+	for i in range(mdt.get_vertex_count()):
+		var vertex = mdt.get_vertex(i)
+		vertex.y = sn.get_noise_3dv(vertex) * noise_height
+		mdt.set_vertex(i, vertex)
+	
+	generate_normals()
+
+	for s in range(array_plane.get_surface_count()):
+		array_plane.surface_remove(s)
+
+	mdt.commit_to_surface(array_plane)
+	
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	st.set_material(material)
+	st.add_smooth_group(true)
+	st.append_from(array_plane, 0, Transform.IDENTITY)
+	
+	#draw_normals(array_plane)
+	
+	$StaticBody/FloorMesh.mesh = st.commit()
+
+	var col_shape = ConcavePolygonShape.new()
+	col_shape.set_faces($StaticBody/FloorMesh.mesh.get_faces())
+	$StaticBody/CollisionShape.set_shape(col_shape)
+	
+func generate_normals():
+	# Calculate vertex normals, face-by-face.
+	for i in range(mdt.get_face_count()):
+		# Get the index in the vertex array.
+		var a = mdt.get_face_vertex(i, 0)
+		var b = mdt.get_face_vertex(i, 1)
+		var c = mdt.get_face_vertex(i, 2)
+		# Get vertex position using vertex index.
+		var ap = mdt.get_vertex(a)
+		var bp = mdt.get_vertex(b)
+		var cp = mdt.get_vertex(c)
+		# Calculate face normal.
+		var n = (bp - cp).cross(ap - bp).normalized()
+		# Add face normal to current vertex normal.
+		# This will not result in perfect normals, but it will be close.
+		mdt.set_vertex_normal(a, n + mdt.get_vertex_normal(a))
+		mdt.set_vertex_normal(b, n + mdt.get_vertex_normal(b))
+		mdt.set_vertex_normal(c, n + mdt.get_vertex_normal(c))
+
+	# Run through vertices one last time to normalize normals and
+	# set color to normal.
+	for i in range(mdt.get_vertex_count()):
+		var v = mdt.get_vertex_normal(i).normalized()
+		print(v)
+		mdt.set_vertex_normal(i, v)
+		mdt.set_vertex_color(i, Color(v.x, v.y, v.z))	
+	
+func draw_normals(array_plane):
+	var error = mdt.create_from_surface(array_plane, 0)
+	
+	for i in range(mdt.get_face_count()):
+		var a = mdt.get_face_vertex(i, 0)
+		var b = mdt.get_face_vertex(i, 1)
+		var c = mdt.get_face_vertex(i, 2)
+		
+		var ap = mdt.get_vertex(a)
+		var bp = mdt.get_vertex(b)
+		var cp = mdt.get_vertex(c)
+		
+		var center = (ap + bp + cp) / 3
+		
+		var normal = mdt.get_face_normal(i) + center
+		print(normal)
+		
+		$DebugNormals.begin(Mesh.PRIMITIVE_LINE_STRIP)
+		$DebugNormals.set_color(Color.purple)
+		$DebugNormals.add_vertex(center)
+		$DebugNormals.add_vertex(normal)
+		$DebugNormals.end()
---
+++ map/terrain/generate_map.gd
@@ -0,0 +1,220 @@
+extends Spatial
+
+onready var grass = preload("res://map/terrain/floor.tscn")
+
+onready var house1 = preload("res://map/houses/scenes/house1.tscn")
+onready var house2 = preload("res://map/houses/scenes/house2.tscn")
+onready var house3 = preload("res://map/houses/scenes/house3.tscn")
+onready var house4 = preload("res://map/houses/scenes/house4.tscn")
+onready var house5 = preload("res://map/houses/scenes/house5.tscn")
+onready var house6 = preload("res://map/houses/scenes/house6.tscn")
+onready var house7 = preload("res://map/houses/scenes/house7.tscn")
+
+onready var delivery_tracker = get_node("../DeliveryTracker")
+onready var mini_map = get_node("../MiniMap")
+
+export var map_size = 2
+export var tile_size = 6
+
+var map_to_road_ratio = 6
+var scalar = 0
+
+var road_grid = null
+
+var house_scenes = []
+var tiles = []
+var houses = []
+
+var unset_houses = []
+var prev_unset_houses = 0
+
+var delivery_zones = []
+
+var mid_matrix = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+	scalar = tile_size / map_to_road_ratio
+	print(scalar)
+
+	$Floor.transform.origin = Vector3(tile_size / 2 * scalar + tile_size, -.95, tile_size / 2 * scalar + tile_size)
+	$Floor.size = scalar * 72
+	$Floor.generate_mesh()
+
+	print ("FLOOORRRR SEEETTTT")
+
+	road_grid = $GridMaps/RoadGridMap
+	house_scenes = [house1, house2, house3, house4, house5, house6, house7]
+
+	$GridMaps.transform.origin = Vector3(scalar,-.95, scalar)
+	$GridMaps.global_scale(Vector3(scalar, 1, scalar))
+
+	for i in range(map_size):
+		mid_matrix.append([])
+
+		for j in range(map_size):
+			add_tile(i, j)
+
+	calculate_roads()
+
+	if delivery_tracker:
+		delivery_tracker.add_zones(houses)
+
+	if mini_map:
+		mini_map.add_houses(houses, tiles)
+	
+	
+func add_tile(i, j):
+
+	var middle_road = 7#randi() % 16
+	mid_matrix[i].append(middle_road)
+
+	var g = Vector3(-1 + i * map_to_road_ratio, 0, -1 + j * map_to_road_ratio)
+
+	road_grid.set_cell_item(g.x, g.y, g.z, middle_road)
+
+func calculate_roads():
+	for i in range(map_size):
+		for j in range(map_size):
+			set_tile_roads(i, j)
+
+	unset_houses = houses
+	print(houses.size())
+
+func set_tile_roads(i, j):
+	var m = mid_matrix[i][j]
+
+	var house_probability = 1
+
+	var x_scalar = i * map_to_road_ratio
+	var z_scalar = j * map_to_road_ratio
+
+	var up_locations = [
+		Vector3(-2, -3, PI / 2),
+		Vector3(0, -3, 3 * PI / 2),
+	]
+
+	var down_locations = [
+		Vector3(-2, 2, PI / 2),
+		Vector3(-2, 1, PI / 2),
+		Vector3(0, 2, 3 * PI / 2),
+		Vector3(0, 1, 3 * PI / 2)
+	]
+
+	var left_locations = [
+		Vector3(-3, -2, 0),
+		Vector3(-4, -2, 0),
+		Vector3(-3, 0, PI),
+		Vector3(-4, 0, PI),
+	]
+
+	var right_locations = [
+		Vector3(1, -2, 0),
+		Vector3(1, 0, PI),
+	]
+
+	if isUp(m):
+		road_grid.set_cell_item(-1 + x_scalar, 0, -3 + z_scalar, 0)
+		for location in up_locations:
+			if (randf() > house_probability):
+				continue
+			createHouse(
+				location.x + x_scalar,
+				location.y + z_scalar,
+				randi() % house_scenes.size(),
+				location.z)
+
+	if isDown(m):
+		road_grid.set_cell_item(-1 + x_scalar, 0, 1 + z_scalar, 0)
+		for location in down_locations:
+			if (randf() > house_probability):
+				continue
+			createHouse(
+				location.x + x_scalar,
+				location.y + z_scalar,
+				randi() % house_scenes.size(),
+				location.z)
+
+	if isLeft(m):
+		road_grid.set_cell_item(-3 + x_scalar, 0, -1 + z_scalar, 12)
+		for location in left_locations:
+			if (randf() > house_probability):
+				continue
+			createHouse(
+				location.x + x_scalar,
+				location.y + z_scalar,
+				randi() % house_scenes.size(),
+				location.z)
+	
+	if isRight(m):
+		road_grid.set_cell_item(1 + x_scalar, 0, -1 + z_scalar, 12)
+		for location in right_locations:
+			if (randf() > house_probability):
+				continue
+			createHouse(
+				location.x + x_scalar,
+				location.y + z_scalar,
+				randi() % house_scenes.size(),
+				location.z)
+
+func isUp(m):
+	return  m == 0 || m == 3 || m == 4 || m == 6 || m == 7 || m == 8 || m == 9 || m == 10
+
+func isDown(m):
+	return  m < 3 || m == 5 || m == 7 || m == 8 || m == 9 || m == 11
+
+func isLeft(m):
+	return m == 2 || m == 4 || m == 7 || m == 10 || m == 11 || m == 12 || m == 14
+
+func isRight(m):
+	return m == 1 || m == 3 || m == 7 || m == 8 || m == 10 || m == 11 || m == 12 || m == 13
+
+func createHouse(x, z, house_index, angle):
+	var house_grid_location = road_grid.map_to_world(x, 3, z)
+	var house_global_location = road_grid.to_global(house_grid_location)
+
+	var house_instance = house_scenes[house_index].instance()
+
+	house_instance.translation = house_global_location
+
+	house_instance.rotate_y(angle)
+
+	add_child(house_instance)
+	houses.append(house_instance)
+
+
+func _physics_process(_delta):
+	if unset_houses.size() == 0 or prev_unset_houses == unset_houses.size():
+		for house in unset_houses:
+			house.visible = false
+			for child in house.get_children():
+				if child is StaticBody:
+					child.collision_layer = 0
+					child.collision_mask = 0
+		
+		unset_houses.clear()
+
+		return
+
+	print("HOUSES UNSET ", unset_houses.size())
+	prev_unset_houses = unset_houses.size()
+	var i = 0
+	var j = 0
+	while i < unset_houses.size():
+		i += 1
+
+		var house = unset_houses[j]
+
+		house.set_position()
+
+		if house.position_set:
+			unset_houses.remove(j)
+		else:
+			j += 1
+
+	 print("HOUSES UNSET ", unset_houses.size())
+
+			
+	
